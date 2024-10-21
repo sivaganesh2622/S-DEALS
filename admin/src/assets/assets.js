ab60a9b3c7e47b0adc630f84fe2d8f78836b8bb4
siva <110612891+sivaganesh2622@users.noreply.github.com>
2024-10-21 20:34:29 +0530

    first commit

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,23 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.svg'
+import sad from './sad.png'
+import logop from './logop.png'
+import plogo from './plogo.png'
+import logod from './logod.png'
+
+export const assets = {
+    logo,
+    sad,
+    logod,
+    plogo,
+    logop,
+    add_icon,
+    plogo,
+    order_icon,
+    upload_area,
+    parcel_icon
+}
+
